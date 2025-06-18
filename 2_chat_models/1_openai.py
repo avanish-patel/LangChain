154efa17feb4098a2de3c57c9d024ab3cacdf60c
Avanish Patel <patelabout@gmail.com>
2025-06-18 16:42:37 -0500

    Added openai LLM model & openai,anthropic and gemini Chat models

---
+++ 2_chat_models/1_openai.py
@@ -0,0 +1,11 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatOpenAI(model="gpt-4", temperature=0.5, max_completion_tokens=20)
+
+result = model.invoke("What is a capital of United States of America?")
+
+print(result.content)
+
